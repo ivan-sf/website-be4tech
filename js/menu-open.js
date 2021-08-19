0020e5ddb358e32d101d6e03c15aeb282f8a4147
MajorTom <ivansantander2020@gmail.com>
2021-08-19 14:12:54 -0500

    test

--- js/menu-open.js
+++ js/menu-open.js
@@ -1,20 +1,14 @@
 $( ".menu-header" ).click(function() {
     $('body,html').animate({scrollTop : 0}, 500);
     $('.body-overflow').css('overflow','hidden');
-    gsap.set('.menu-open',{
-        y:"-180vh",delay:1
-    })
-    gsap.to('.menu-open',{
+        gsap.to('.menu-open',{
         duration: 1, ease: "circ", opacity:1,y:'180vh',delay:1
     })
   });
 
   $( ".close-menu" ).click(function() {
     $('.body-overflow').css('overflow','visible');
-    gsap.set('.menu-open',{
-        opacity: 0,y:'180vh',delay:1
-    })
     gsap.to('.menu-open',{
-        duration: 5, ease: "circ", opacity:1,y:'-180vh',delay:1
+        duration: 5, ease: "circ", opacity:0,y:'-180vh'
     })
   })
