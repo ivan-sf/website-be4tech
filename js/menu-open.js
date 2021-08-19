e8a4e5c9619ff04a66144351f027f9a07faa9425
MajorTom <ivansantander2020@gmail.com>
2021-08-19 14:35:41 -0500

    test

--- js/menu-open.js
+++ js/menu-open.js
@@ -3,11 +3,13 @@ $( ".menu-header" ).click(function() {
     gsap.to('.menu-open',{
         duration: 1, ease: "circ", opacity:1,y:'180vh',delay:1
     })
+    $('body,html').animate({scrollTop : 0}, 500);
+    return false
   });
 
   $( ".close-menu" ).click(function() {
     $('.body-overflow').css('overflow','visible');
     gsap.to('.menu-open',{
-        duration: 1, ease: "circ", opacity:1,y:'-180vh',delay:1
+        duration: 3, ease: "circ", opacity:1,y:'-180vh',delay:1
     })
   })
