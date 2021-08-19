b64b26ed416636c950b40b5d168933c442b66216
MajorTom <ivansantander2020@gmail.com>
2021-08-19 11:39:50 -0500

    test

--- js/menu-open.js
+++ js/menu-open.js
@@ -4,6 +4,6 @@ $( ".menu-header" ).click(function() {
         opacity: 0,y:0
     })
     gsap.to('.menu-open',{
-        duration: 1, ease: "circ", opacity:1,y:'100vh'
+        duration: 1, ease: "circ", opacity:1,y:'150vh'
     })
   });
