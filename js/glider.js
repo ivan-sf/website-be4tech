c27ae534b5ce89faa8d4d78d9029f3a1f52f96a1
MajorTom <ivansantander2020@gmail.com>
2021-08-23 11:11:59 -0500

    test

--- js/glider.js
+++ js/glider.js
@@ -1,12 +1,31 @@
 window.addEventListener('load', function(){
-  new Glider(document.querySelector('.carousel__lista'), {
-    slidesToShow: 1,
-    dots: '.carousel__indicadores',
+	new Glider(document.querySelector('.carousel__lista'), {
+		slidesToShow: 1,
+		slidesToScroll: 1,
+    slidesToShow: 'auto',
     draggable: true,
-    arrows: {
-      prev: '.carousel__anterior',
-      next: '.carousel__siguiente'
-    }
-  });
-	
+		dots: '.carousel__indicadores',
+		arrows: {
+			prev: '.carousel__anterior',
+			next: '.carousel__siguiente'
+		},
+		responsive: [
+			{
+			  // screens greater than >= 775px
+			  breakpoint: 320,
+			  settings: {
+				// Set to `auto` and provide item width to adjust to viewport
+				slidesToShow: 1,
+				slidesToScroll: 1
+			  }
+			},{
+			  // screens greater than >= 1024px
+			  breakpoint: 800,
+			  settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1
+			  }
+			}
+		]
+	});
 });
