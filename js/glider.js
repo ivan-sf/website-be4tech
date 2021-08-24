f8ffb2fb054a98125f599b30a32a8faf0ecd5cac
MajorTom <ivansantander2020@gmail.com>
2021-08-24 11:46:55 -0500

    test

--- js/glider.js
+++ js/glider.js
@@ -14,8 +14,8 @@ window.addEventListener('load', function(){
 			  	breakpoint: 320,
 			  	settings: {
 				// Set to `auto` and provide item width to adjust to viewport
-				slidesToShow: 1.1,
-				slidesToScroll: 1.1,
+				slidesToShow: 1,
+				slidesToScroll: 3,
 				draggable: true,
 				scrollLock: true,
 			  }
