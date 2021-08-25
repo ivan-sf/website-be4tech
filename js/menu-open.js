ca7be4e8abffe749277bed7ff2968c449ecfed34
MajorTom <ivansantander2020@gmail.com>
2021-08-25 13:02:33 -0500

    test

--- js/menu-open.js
+++ js/menu-open.js
@@ -46,3 +46,64 @@ $( ".menu-header" ).click(function() {
      });
      return false;
   });
+
+
+  $(".title-icon-iot").hover(function(){
+    gsap.to('.icon-img-iot',{
+      ease: "circ",y:-25,
+     })
+    gsap.from('.content-iot',{
+      x:-250,y:-280
+    })
+    gsap.to('.content-iot',{
+    ease: "circ",visibility:'visible',opacity:1,x:70,y:-280
+    })
+  }, function(){
+    gsap.to('.icon-img-iot',{
+      ease: "circ",y:0,delay:0.3
+     })
+    gsap.to('.content-iot',{
+      ease: "circ",visibility:'visible',opacity:0,x:0,y:-280
+     });
+     return false;
+  });
+
+  $(".title-icon-ia").hover(function(){
+    gsap.to('.icon-img-ia',{
+      ease: "circ",y:-25,
+     })
+    gsap.from('.content-ia',{
+      x:-250,y:-160
+    })
+    gsap.to('.content-ia',{
+    ease: "circ",visibility:'visible',opacity:1,x:70,y:-170
+    })
+  }, function(){
+    gsap.to('.icon-img-ia',{
+      ease: "circ",y:0,delay:0.3
+     })
+    gsap.to('.content-ia',{
+      ease: "circ",visibility:'visible',opacity:0,x:0,y:-170
+     });
+     return false;
+  });
+
+  $(".title-icon-bd").hover(function(){
+    gsap.to('.icon-img-bd',{
+      ease: "circ",y:-25,
+     })
+    gsap.from('.content-bd',{
+      x:-250,y:-60
+    })
+    gsap.to('.content-bd',{
+    ease: "circ",visibility:'visible',opacity:1,x:70,y:-60
+    })
+  }, function(){
+    gsap.to('.icon-img-bd',{
+      ease: "circ",y:0,delay:0.3
+     })
+    gsap.to('.content-bd',{
+      ease: "circ",visibility:'visible',opacity:0,x:0,y:-60
+     });
+     return false;
+  });
