68ade363073822e0df4689a29a0531a449583268
MajorTom <ivansantander2020@gmail.com>
2021-08-26 11:45:36 -0500

    test

--- js/gsap.js
+++ js/gsap.js
@@ -16,7 +16,7 @@ gsap.set('.img-2-banner',{
     duration: 0, x: 0, y: '-80vh', opacity: 0
 })
 gsap.to('.img-2-banner',{
-    duration: 1.5, ease: "bounceInTop", opacity: 1, delay: 4, y:0
+    duration: 1.5, ease: "bounceInTop", opacity: .4, delay: 4, y:0
 })
 
   
